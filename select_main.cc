e58b6e562bb87dfd244067bbe4656864ea433084
Joe Zane <joezane.cn@gmail.com>
2022-06-24 00:11:49 +0800

    1. add unistd.h

--- select_main.cc
+++ select_main.cc
@@ -2,15 +2,22 @@
 #include <sstream>
 #include <stdio.h>
 #include <cassert>
+#include <unistd.h>
 #include "select.h"
 
-#define STDIN_FILENO 0
+// #define STDIN_FILENO 0
 
 using namespace std;
 
 int main(int argc, const char** argv) {
     /* prepare to poll for events */
     Select &sel = Select::get_instance();
+    sel.add_signal(SIGWINCH);
+    sel.add_signal(SIGTERM);
+    sel.add_signal(SIGINT);
+    sel.add_signal(SIGHUP);
+    sel.add_signal(SIGPIPE);
+    sel.add_signal(SIGCONT);
 
     sel.read(STDIN_FILENO);
 
